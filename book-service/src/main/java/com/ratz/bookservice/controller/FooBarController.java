f2cdaba3918af44ff5eec1d3a9d642671bc06e41
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-13 14:09:07 +0000

    added resilience4j . created foo controller to test and implement errors on purpose

---
+++ book-service/src/main/java/com/ratz/bookservice/controller/FooBarController.java
@@ -0,0 +1,16 @@
+package com.ratz.bookservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+  @GetMapping("foo-bar")
+  public String fooBar(){
+
+    return "foo bar";
+  }
+}
