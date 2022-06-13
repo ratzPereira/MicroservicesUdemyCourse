6a11572fea63b9dd85129abe6e7db78ba8693d5c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-13 12:39:43 +0000

    created api gateway service

--- book-service/src/main/java/com/ratz/bookservice/proxy/ExchangeProxy.java
+++ book-service/src/main/java/com/ratz/bookservice/proxy/ExchangeProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "exchange-service")
 public interface ExchangeProxy {
 
 
