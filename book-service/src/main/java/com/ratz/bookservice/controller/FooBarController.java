b4da7404b86c29c5033ae3b3dbd0ca5e3a88527d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-13 14:28:10 +0000

    added @Retry and changed it to 5 attempts .

--- book-service/src/main/java/com/ratz/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/ratz/bookservice/controller/FooBarController.java
@@ -1,16 +1,30 @@
 package com.ratz.bookservice.controller;
 
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
+  Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
   @GetMapping("foo-bar")
+  @Retry(name = "foo-bar")
   public String fooBar(){
 
-    return "foo bar";
+    logger.info("Request to foo bar is refused!!");
+
+    ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+
+    return response.getBody();
+    //return "foo bar";
   }
 }
