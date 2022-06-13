cb48d199510e843545b5b3b3d9a5a800841af444
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-13 13:14:48 +0000

    changed api gateway configs, added router  locator

---
+++ API-gateway/src/main/java/com/ratz/APIgateway/config/APIGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.ratz.APIgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class APIGatewayConfiguration {
+
+  @Bean
+  public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+    return builder.routes()
+        .route(p -> p.path("/exchange-service/**").uri("lb://exchange-service"))
+        .route(p -> p.path("/book-service/**").uri("lb://book-service"))
+        .build();
+  }
+}
