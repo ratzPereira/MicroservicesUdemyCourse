7331e64a598c4ed382c51f2496180c62abf82f51
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-14 13:38:57 +0000

    adjusted open api configs, now we can check the documentation of each microservice using api gateway

--- API-gateway/src/main/java/com/ratz/APIgateway/config/APIGatewayConfiguration.java
+++ API-gateway/src/main/java/com/ratz/APIgateway/config/APIGatewayConfiguration.java
@@ -1,8 +1,5 @@
 package com.ratz.APIgateway.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
